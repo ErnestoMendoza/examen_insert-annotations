3bb9137f5a554300efcffabb302622e83bfb2a12
Ernesto Mendoza <cacho_mexico_93@hotmail.com>
2018-07-02 12:20:35 -0500

    Agregar archivos de proyecto.

---
+++ Examen_Planet_1.0v/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Examen_Planet_1._0v
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Examen_Planet_1.0v/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Examen_Planet_1._0v.MvcApplication" Language="C#" %>
